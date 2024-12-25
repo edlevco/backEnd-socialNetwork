9a35f46bb5c2ede9d684b0588e0bdd719c28e20c
edlevco <Edlevco88@gmail.com>
2024-12-25 11:39:02 -0500

    Added chats and notifications with "read" function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from server import Server
+from termcolor import colored
 
 server = Server()
 
@@ -8,39 +9,41 @@ def __main__():
     while server_on:
         joining = True
         while joining:
-            print("Welcome to Pinstagram")
+            print(colored("\nWelcome to PInstagram", "blue"))
 
-            joinOption = getValidInt("\n(1) Sign up\n(2) Log in\n", 1, 2)
+            joinOption = getValidInt("\n(1) Sign up\n(2) Log in\n\n", 1, 2)
             
             if joinOption == 1:
                 self = server.make_new_client()
+                server.joinNotification(self["username"])
                 joining = False
                 break
             elif joinOption == 2:
                 self = server.client_login()
                 if (self != None): # if a client has been found, enter
                     joining = False
-                else:
-                    print("Incorrect username or password: Try again. \n")
 
-        print("Welcome to Instagram: "+ self["username"])
+        print(colored("\nWelcome to PInstagram: "+ self["username"]+"\n", "blue"))
 
         isLoggedIn = True
         connected_client = None
         
         while isLoggedIn:
-            mainOption = getValidInt("(1) Search Accounts\n(2) View Chats\n(3) View Notifications\n(4) Sign Out\n", 1, 4)
+            newNotifications = server.get_unread(self["username"], "notifications") ## if not 0 will look like (5 new notifications): if zero will be None
+            newChats = server.get_unread(self["username"], "chats")
+
+            mainOption = getValidInt(f"\n(1) Search Accounts\n(2) View Chats {newChats}\n(3) View Notifications {newNotifications}\n{colored("(4) Sign Out", "red")}\n", 1, 4)
 
             if mainOption == 1:
-                accountUsername = input("Search for account with username: ").lower()
+                accountUsername = input("Search for account with username: ").capitalize()
                 connected_client = server.get_client(accountUsername)
                 if connected_client == None:
-                    print("No user found with username: "+ accountUsername)
+                    print(colored("\nNo user found with username: "+ accountUsername, "yellow"))
                 else:
                     onProfile = True
                     while onProfile:
                         server.print_user_stats(self, accountUsername)
-                        profileOptions = getValidInt("(1) Follow/ Unfollow\n(2) Send Message\n(3) Return To Home\n\n", 1, 3)
+                        profileOptions = getValidInt(f"(1) Follow/ Unfollow\n(2) Send Message\n{colored("(3) Return Home", "red")}\n\n", 1, 3)
 
                         if profileOptions == 1:
                             server.follow_user(self, connected_client)
@@ -48,6 +51,10 @@ def __main__():
                             server.send_chat(self, connected_client)
                         elif profileOptions == 3:
                             onProfile = False
+            if mainOption == 2:
+                server.print_user_chats(self)
+            if mainOption == 3:
+                server.print_user_notifications(self)
 
             elif mainOption == 4:
                 isLoggedIn = False
@@ -62,8 +69,9 @@ def getValidInt(prompt, min, max):
             if min <= num <= max:  # Check if the number is within range
                 return num
             else:
-                print(f"Error: Please enter an integer between {min} and {max}.")
+                print(colored(f"Error: Please enter an integer between {min} and {max}.", "red"))
         except ValueError:
-            print("Error: Invalid input. Please enter a valid integer.")
+            print(colored("Error: Invalid input. Please enter a valid integer.", "red"))
 
 __main__()
+
--- server.py
+++ server.py
@@ -4,16 +4,13 @@ import json
 import base64
 from termcolor import colored
 
-
-
 FILE_PATH = "clients_json.json"
-
 
 class Server:
     
     def __init__(self):
-        print(colored("Hello", "red"))
         self.initialize_json_file()
+        
 
     def make_new_client(self):
         """Create a new client."""
@@ -23,14 +20,14 @@ class Server:
             hashed_pass = self.hash_password(password)
 
             if self.add_client(username, hashed_pass):
-                print("\nSuccessful Sign Up\n")
+                print(colored("\nSign Up Successful!\n", "green"))
                 return self.get_client(username)
             else:
-                print("Username already in use: Try Again\n")
+                print(colored("Username already in use: Try Again\n", "yellow"))
 
     def make_username(self):
         """Prompt user for a unique username."""
-        return input("Enter a unique username: ")
+        return input("Enter a unique username: ").capitalize()
 
     def get_password(self):
         """Prompt user for a password."""
@@ -39,26 +36,28 @@ class Server:
     def client_login(self):
         """Log in an existing client."""
         data = self.load_json()
-        username = input("Enter your username: ")
+        username = input("Enter your username: ").capitalize()
+
+        
 
         if username in data["clients"]:
             password = input("Enter your password: ")
             stored_hash = base64.b64decode(data["clients"][username]["password"])
 
             if bcrypt.checkpw(password.encode(), stored_hash):
-                print("Login Successful!")
+                print(colored("Login Successful!", "green"))
                 return data["clients"][username]
             else:
-                print("Invalid password.")
+                print(colored("\nInvalid password.", "red"))
                 return None
         else:
-            print("Username not found.")
+            print(colored("\nUsername not found.", "red"))
             return None
 
     def get_client(self, username):
         """Retrieve a client by username."""
         data = self.load_json()
-        return data["clients"].get(username, None)
+        return data["clients"].get(username.capitalize(), None)
 
     def hash_password(self, password):
         """Hash a password using bcrypt."""
@@ -68,6 +67,7 @@ class Server:
     def add_client(self, username, password):
         """Add a new client to the JSON file."""
         data = self.load_json()
+
 
         if username in data["clients"]:
             return False
@@ -98,6 +98,18 @@ class Server:
         if not os.path.exists(FILE_PATH):
             with open(FILE_PATH, "w") as f:
                 json.dump({"clients": {}}, f, indent=4)
+
+            populated_array = self.make_array(9)
+            data = self.load_json()
+            data["clients"]["Pinstagram"] = {
+                "username": "Pinstagram",
+                "password": self.hash_password("weoifjwoifjoijoijoijoij"),
+                "followers": populated_array,
+                "notifications": [],
+                "chats": []
+            }
+            self.save_json(data)
+                
     
     def follow_user(self, follower, followed):
         follower_username = follower["username"]
@@ -107,15 +119,15 @@ class Server:
         followed = data["clients"].get(followed_username)
 
         if follower["username"] == followed["username"]:
-            print("You cannot follow yourself")
+            print(colored("You cannot follow yourself", "yellow"))
         elif follower["username"] in followed["followers"]:
-            print(f"You have unfollowed {followed["username"]}")
-            followed["notifications"].append(f"{follower["username"]} has unfollowed you.")
+            print(colored(f"\nYou have unfollowed {followed["username"]}", "blue"))
+            followed["notifications"].append({"message": f"{follower["username"]} has unfollowed you.", "isRead": False})
             followed["followers"].remove(follower["username"])
         else:
-            followed["notifications"].append(f"{follower["username"]} has followed you.", "hello")
+            followed["notifications"].append({"message": f"{follower["username"]} has followed you.", "isRead": False})
             followed["followers"].append(follower["username"])
-            print(f"You have followed {followed["username"]}")
+            print(colored(f"\nYou have followed {followed["username"]}", "green"))
         self.save_json(data)
     
     def send_chat(self, sender, recipient):
@@ -126,24 +138,83 @@ class Server:
         recipient = data["clients"].get(recipient_username)
 
         message = input(f"To {recipient["username"]}:\n{sender["username"]}: ")
-        recipient["notifications"].append(f"{sender["username"]} has sent you a message.")
-        recipient["chats"].append([sender["username"], message])
+        recipient["notifications"].append({"message": f"{sender["username"]} has sent you a message.", "isRead": False})
+        recipient["chats"].append({"from": sender["username"], "message": message, "isRead": False})
         self.save_json(data)
+
+        print(colored(f"\nMessage sent to {recipient_username}", "green"))
 
     def print_user_stats(self, current, username):
         data = self.load_json()
         followers = data["clients"].get(username)["followers"]
         follow_status = None
         if current["username"] in followers:
-            follow_status = "Following"
+            follow_status = colored("Following", "green")
         else:
-            follow_status = "Follow"
+            follow_status = colored("Follow", "blue")
 
-        print(f"\nAccount: {username}\nFollowers: {len(data["clients"][username]["followers"])}\n{follow_status}")
+        print(f"\nAccount: {username}\nFollowers: {len(data["clients"][username]["followers"])}\n{follow_status}\n")
+
+    def get_unread(self, username, category):
+        unread_count = 0
+        data = self.load_json()
+        user = data["clients"].get(username)
+
+        for thing in user[category]:
+            if thing["isRead"] == False:
+                unread_count += 1
+        
+        if unread_count == 0: ## if they have no new notifications return nothing
+            return ""
+        else: ## if they have a new notification return a string to display
+            return colored(f"({unread_count} new {category})", "blue")
+    
+    def print_user_notifications(self, user):
+        data = self.load_json()
+        username = user["username"]
+        user = data["clients"].get(username)
+
+        if user["notifications"] == []:
+            print(colored("\nYou have 0 notifications", "yellow"))
+        else:
+            print(f"\n{username}'s notifications\n")
+            for notification in user["notifications"]:
+                if notification["isRead"]:
+                    print(notification["message"])
+                else:
+                    print(colored(f"NEW: {notification["message"]} ", "blue"))
+                    notification["isRead"] = True
+        
+        self.save_json(data)
+
+    def print_user_chats(self, user):
+        data = self.load_json()
+        username = user["username"]
+        user = data["clients"].get(username)
+
+        if user["notifications"] == []:
+            print(colored("\nYou have 0 chats", "yellow"))
+        else:
+            print(f"\n{username}'s chats\n")
+            for chat in user["chats"]:
+                if chat["isRead"]:
+                    print(f"{chat["from"]}: {chat["message"]}")
+                else:
+                    print(colored(f"{chat["from"]}: {chat["message"]} (NEW)", "blue"))
+                    chat["isRead"] = True
+        self.save_json(data)
+
+    def joinNotification(self, username):
+        data = self.load_json()
+        user = data["clients"].get(username)
+
+        user["notifications"].append({"message": "PInstagram has sent you a message.", "isRead": False})
+        user["chats"].append({"from": "PInstagram", "message": "Welcome to PInstagram!!", "isRead": False})
+
+        self.save_json(data)
+    
+    def make_array(self, size):
+        return ["filler_user" for a in range(size)]
 
 
-        ## add read vs unread status
-        ## add color
-        # notifications [{message, status}]
-        # chats [sender, message, status]
     
